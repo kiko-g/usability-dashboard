9f84bdcfd67f2c8d48c9bfb1d438e3256df3168f
kiko-g <kikojpgoncalves@gmail.com>
2023-04-28 23:10:20 +0100

    add: date range

--- pages/api/matomo/events.ts
+++ pages/api/matomo/events.ts
@@ -7,8 +7,12 @@ const matomoToken = '9a7c2dea66b81a8a903c10a06ebcd5e0';
 
 export default function matomoApiTest(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
-    const period = 'week'; // day, week, month, year, range
-    const date = '2023-04-28'; // YYYY-MM-DD
+    const period = 'range'; // day, week, month, year, range
+
+    const currentDate = new Date();
+    const formattedCurrentDate = `${currentDate.getFullYear()}-${(currentDate.getMonth() + 1).toString().padStart(2, '0')}-${currentDate.getDate().toString().padStart(2, '0')}`;
+    const date = `2023-01-01,${formattedCurrentDate}`;
+    
     const method = 'Events.getCategory';
     const format = 'json';
     const apiUrl = `${matomoUrl}/index.php?module=API&method=${method}&expanded=1&format=${format}&idSite=${siteId}&period=${period}&date=${date}&token_auth=${matomoToken}`;
