d15871a69da7cc309eac1e30786b7f7d73bb16b3
kiko-g <kikojpgoncalves@gmail.com>
2023-05-04 00:21:15 +0100

    refactor: grouped code moved

--- pages/api/matomo/events/grouped/index.ts
+++ pages/api/matomo/events/grouped/index.ts
@@ -2,25 +2,23 @@ import request from 'request';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { config, parseAndGroupEvents } from '../../../../../utils/matomo';
 
-export default function eventsGroupedMatomoApi(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'GET') {
-    const period = 'range'; // day, week, month, year, range
-    const date = `2023-04-29,today`; // YYYY-MM-DD
-    const apiUrl = `${config.matomoSiteUrl}/index.php?module=API&method=Events.getCategory&secondaryDimension=eventAction&flat=1&format=json&idSite=${config.matomoSiteId}&period=${period}&date=${date}&token_auth=${config.matomoToken}`;
+export default function eventsGroupedWizardsMatomoApi(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'GET') return res.status(405).json({ error: 'Method Not Allowed' })
 
-    request(apiUrl, { json: true }, (err, response, body) => {
-      if (err) {
-        return res.status(500).json({ error: 'Internal server error' });
-      }
+  const period = 'range'; // day, week, month, year, range
+  const date = `2023-04-29,today`; // YYYY-MM-DD
+  const apiUrl = `${config.matomoSiteUrl}/index.php?module=API&method=Events.getCategory&secondaryDimension=eventAction&flat=1&format=json&idSite=${config.matomoSiteId}&period=${period}&date=${date}&token_auth=${config.matomoToken}`;
 
-      if (response.statusCode !== 200 || body.result === 'error') {
-        return res.status(response.statusCode).json({ error: 'Error from Matomo API', message: body.message });
-      }
+  request(apiUrl, { json: true }, (err, response, body) => {
+    if (err) {
+      return res.status(500).json({ error: 'Internal server error' });
+    }
 
-      const groupedEvents = parseAndGroupEvents(body);
-      return res.status(200).json(groupedEvents);
-    });
-  } else {
-    return res.status(405).json({ error: 'Method Not Allowed' });
-  }
+    if (response.statusCode !== 200 || body.result === 'error') {
+      return res.status(response.statusCode).json({ error: 'Error from Matomo API', message: body.message });
+    }
+
+    const groupedEvents = parseAndGroupEvents(body);
+    return res.status(200).json(groupedEvents);
+  });
 }
