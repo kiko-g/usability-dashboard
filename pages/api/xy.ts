6fe725ad1cd8bcd614e9fed3aa9fb06cd20ebea5
kiko-g <kikojpgoncalves@gmail.com>
2023-05-04 12:11:29 +0100

    add: test xy route

---
+++ pages/api/xy.ts
@@ -0,0 +1,11 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = {
+  x: number[];
+  y: number[];
+};
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  res.status(200).json({ x: [1, 2, 3, 4, 5], y: [2, 4, 6, 8, 10] });
+}
