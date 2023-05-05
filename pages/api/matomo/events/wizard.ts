08480310ebb5dada57c19ca0d5956a5b9c71a24d
kiko-g <kikojpgoncalves@gmail.com>
2023-05-05 14:46:44 +0100

    fix: bad import

--- pages/api/matomo/events/wizard.ts
+++ pages/api/matomo/events/wizard.ts
@@ -1,6 +1,6 @@
 import request from 'request';
 import { NextApiRequest, NextApiResponse } from 'next';
-import { config, parseAndGroupEvents } from '../../../../../utils/matomo';
+import { config, parseAndGroupEvents } from '../../../../utils/matomo';
 
 export default function eventsGroupedWizardsMatomoApi(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'GET') return res.status(405).json({ error: 'Method Not Allowed' })
