318fec80dcf26cf60b4378860a7a8ec764f13221
kiko-g <kikojpgoncalves@gmail.com>
2023-05-11 20:27:47 +0100

    fix: views route

--- pages/api/matomo/views.ts
+++ pages/api/matomo/views.ts
@@ -13,8 +13,8 @@ type ResponseType = VisitsPerDay[] | CustomAPIError;
 export default function getVisitsPerDay(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
   if (req.method !== 'GET') res.status(405).json({ error: 'Method Not Allowed' });
 
-  const period = 'range'; // day, week, month, year, range
-  const date = `2023-04-29,today`; // YYYY-MM-DD
+  const period = 'day';
+  const date = `2023-04-29,today`;
   const apiUrl = `${config.matomoSiteUrl}/index.php?module=API&method=VisitsSummary.get&format=json&idSite=${config.matomoSiteId}&period=${period}&date=${date}&token_auth=${config.matomoToken}`;
 
   request(apiUrl, { json: true }, (err, response, body) => {
@@ -26,9 +26,9 @@ export default function getVisitsPerDay(req: NextApiRequest, res: NextApiRespons
       res.status(response.statusCode).json({ error: 'Error from Matomo API', message: body.message });
     }
 
-    const visitsPerDay: VisitsPerDay[] = Object.entries(body).map(([day, visitCount]) => ({
-      day,
-      visitCount: parseInt(visitCount as string),
+    const visitsPerDay: VisitsPerDay[] = body.map((dayData: any) => ({
+      day: dayData.date,
+      visitCount: dayData.nb_visits !== null ? dayData.nb_visits : 0,
     }));
 
     res.status(200).json(visitsPerDay);
