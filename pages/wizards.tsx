511c97ba07a1512bb07e8c9c1e85d24ecbb7d234
kiko-g <kikojpgoncalves@gmail.com>
2023-05-15 04:48:03 +0100

    update: circle progress chart size

--- pages/wizards.tsx
+++ pages/wizards.tsx
@@ -49,7 +49,7 @@ function WizardKPIs() {
       });
   }, [willFetch]);
 
-  // calculate average score for every wizard
+  // calculate average score considering all wizards
   const avgScore = React.useMemo<number | null>(() => {
     if (data.length === 0) return null;
 
@@ -57,7 +57,7 @@ function WizardKPIs() {
     return data.length > 0 ? sum / data.length : 0;
   }, [data]);
 
-  // calculate average completion rate for every wizard
+  // calculate average completion rate considering all wizards
   const completionRate = React.useMemo<CompletionRate | null>(() => {
     if (data.length === 0) return null;
 
@@ -72,8 +72,8 @@ function WizardKPIs() {
     };
   }, [data]);
 
-  // calculate average, minimum and maximum time for every wizard
-  const { averageTime, minTime, maxTime } = React.useMemo(() => {
+  // calculate average, minimum and maximum time considering all wizards
+  const wizardTimeStats = React.useMemo(() => {
     const totalTime = data.reduce((acc, item) => acc + item.avgTimespan * item.total, 0);
     const totalCount = data.reduce((acc, item) => acc + item.total, 0);
 
@@ -82,7 +82,35 @@ function WizardKPIs() {
     const maxTime = Math.max(...timespans);
     const averageTime = totalCount > 0 ? totalTime / totalCount : 0;
 
-    return { averageTime, minTime, maxTime };
+    return { avg: averageTime, min: minTime, max: maxTime };
+  }, [data]);
+
+  // calculate average, minimum and maximum step time considering all wizards
+  const stepTimeStats = React.useMemo(() => {
+    let totalStepTime = 0;
+    let stepCount = 0;
+    let minStepTime = 0;
+    let maxStepTime = 0;
+
+    data.forEach((item) => {
+      item.wizards.forEach((wizard) => {
+        for (let i = 0; i < wizard.events.length - 1; i++) {
+          if (wizard.events[i].action === 'Activate' && wizard.events[i + 1].action === 'Success') {
+            const stepTime =
+              (new Date(wizard.events[i + 1].time).getTime() - new Date(wizard.events[i].time).getTime()) / 1000;
+            totalStepTime += stepTime;
+            stepCount++;
+
+            minStepTime = Math.min(minStepTime, stepTime);
+            maxStepTime = Math.max(maxStepTime, stepTime);
+          }
+        }
+      });
+    });
+
+    const averageStepTime = stepCount > 0 ? totalStepTime / stepCount : 0;
+
+    return { avg: averageStepTime, min: minStepTime, max: maxStepTime };
   }, [data]);
 
   // return early if loading
@@ -125,10 +153,21 @@ function WizardKPIs() {
     );
 
   return data.length === 0 ? null : (
-    <div className="mt-3 flex gap-6">
-      {completionRate === null ? null : <WizardCompletionRateCard completion={completionRate} />}
-      {avgScore === null ? null : <WizardAverageUXScoreCard score={avgScore} />}
-      <AverageTimeCard stats={{ avg: averageTime, min: minTime, max: maxTime }} />
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center gap-4">
+        {completionRate === null ? null : <WizardCompletionRateCard completion={completionRate} />}
+        {avgScore === null ? null : <WizardAverageUXScoreCard score={avgScore} />}
+      </div>
+      <div className="flex items-center gap-4">
+        <TimeStatsCard
+          text="Wizard Time Stats"
+          stats={{ avg: wizardTimeStats.avg, min: wizardTimeStats.min, max: wizardTimeStats.max }}
+        />
+        <TimeStatsCard
+          text="Step Time Stats"
+          stats={{ avg: stepTimeStats.avg, min: stepTimeStats.min, max: stepTimeStats.max }}
+        />
+      </div>
     </div>
   );
 }
@@ -141,22 +180,22 @@ type CompletionRate = {
 
 function WizardCompletionRateCard({ completion }: { completion: CompletionRate }) {
   const progress = Math.min(Math.max(completion.ratio, 0), 1) * 100;
-  const diameter = 200;
-  const strokeWidth = 12;
+  const diameter = 120; // Adjusted diameter value
+  const strokeWidth = 7; // Adjusted strokeWidth value
   const radius = (diameter - strokeWidth) / 2;
   const circumference = 2 * Math.PI * radius;
   const offset = circumference - (progress / 100) * circumference;
 
   return (
     <div className="relative max-w-xs overflow-hidden rounded bg-white/80 p-4 dark:bg-white/10">
+      {/* Adjusted max-w value */}
       <h3 className="font-medium text-slate-700 dark:text-slate-100">Wizard Completion Rate</h3>
-      <p className="mt-1 min-h-[6rem] text-sm tracking-tight">
+      <p className="mt-1 min-h-[5rem] text-sm tracking-tight">
         Ratio of wizards that were submitted successfully vs. all the wizards started in the platform
       </p>
-
       {/* Circular Progress */}
-      <div className="mt-4 flex items-center justify-center p-4">
-        <svg className="w-50 h-50" viewBox={`0 0 ${diameter} ${diameter}`} xmlns="http://www.w3.org/2000/svg">
+      <div className="mt-2 flex items-center justify-center p-4">
+        <svg viewBox={`0 0 ${diameter} ${diameter}`} xmlns="http://www.w3.org/2000/svg">
           <circle
             fill="none"
             stroke="currentColor"
@@ -189,23 +228,23 @@ function WizardCompletionRateCard({ completion }: { completion: CompletionRate }
 }
 
 function WizardAverageUXScoreCard({ score }: { score: number }) {
-  const diameter = 200;
-  const strokeWidth = 12;
+  const diameter = 120; // Adjusted diameter value
+  const strokeWidth = 7; // Adjusted strokeWidth value
   const radius = (diameter - strokeWidth) / 2;
   const circumference = 2 * Math.PI * radius;
   const offset = circumference - (score / 100) * circumference;
 
   return (
     <div className="relative max-w-xs rounded bg-white/80 p-4 dark:bg-white/10">
+      {/* Adjusted max-w value */}
       <h3 className="font-medium text-slate-700 dark:text-slate-100">Wizard Average UX Score</h3>
-      <p className="mt-1 min-h-[6rem] text-sm tracking-tight">
+      <p className="mt-1 min-h-[5rem] text-sm tracking-tight">
         Ratio of wizards that were submitted successfully vs. all the wizards started in the platform. Score is
         calculated based on <span className="underline">this approach</span>.
       </p>
-
       {/* Circular Progress */}
-      <div className="mt-4 flex items-center justify-center p-4">
-        <svg className="w-50 h-50" viewBox={`0 0 ${diameter} ${diameter}`} xmlns="http://www.w3.org/2000/svg">
+      <div className="mt-2 flex items-center justify-center p-4">
+        <svg viewBox={`0 0 ${diameter} ${diameter}`} xmlns="http://www.w3.org/2000/svg">
           <circle
             fill="none"
             stroke="currentColor"
@@ -235,12 +274,12 @@ function WizardAverageUXScoreCard({ score }: { score: number }) {
   );
 }
 
-const AverageTimeCard = ({ stats }: { stats: { avg: number; min: number; max: number } }) => {
+const TimeStatsCard = ({ stats, text }: { text: string; stats: { avg: number; min: number; max: number } }) => {
   const { avg, min, max } = stats;
 
   return (
-    <div className="relative max-w-xs rounded bg-white/80 p-4 dark:bg-white/10">
-      <h2 className="mb-4 text-xl font-bold">Wizard Time Stats</h2>
+    <div className="relative max-w-sm rounded bg-white/80 p-4 dark:bg-white/10">
+      <h2 className="mb-4 text-xl font-bold">{text}</h2>
       <div className="flex items-center gap-x-2">
         <span className="h-4 w-4 rounded-full bg-cyan-500" />
         <span className="font-semibold">
