b7cd53005b5d2da6c7c0df19a791975149ef2a33
kiko-g <kikojpgoncalves@gmail.com>
2023-05-19 19:59:34 +0100

    add: axios all

--- pages/api/matomo/visits.ts
+++ pages/api/matomo/visits.ts
@@ -5,41 +5,52 @@ import type { CustomAPIError } from '../../../@types';
 
 type ResponseType = any | CustomAPIError;
 
-const requestAndReturn = async (apiUrl: string) => {
-  try {
-    const response = await axios.get(apiUrl);
-    if (response.status !== 200 || response.data.result === 'error') {
-      throw { error: 'Error from Matomo API', message: response.data.message };
-    }
-
-    return response.data;
-  } catch (err: any) {
-    throw { error: 'Internal server error', ...err };
-  }
-};
-
 export default async function getAllEvents(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
-  if (req.method !== 'GET') return res.status(405).json({ error: 'Method Not Allowed' });
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Method Not Allowed' });
+  }
 
   const period = 'range'; // day, week, month, year, range
   const date = `2023-04-29,today`; // YYYY-MM-DD
-  const pagesApiUrl = `${config.matomoSiteUrl}/index.php?module=API&format=JSON&idSite=${config.matomoSiteId}&period=${period}&date=${date}&method=Actions.getPageUrls&expanded=1&token_auth=${config.matomoToken}&filter_limit=-1`;
-  const osApiUrl = `${config.matomoSiteUrl}/index.php?module=API&method=DevicesDetection.getOsFamilies&format=json&idSite=${config.matomoSiteId}&period=${period}&date=${date}&token_auth=${config.matomoToken}&filter_limit=-1`;
-  const browsersApiUrl = `${config.matomoSiteUrl}/index.php?module=API&method=DevicesDetection.getBrowsers&format=json&idSite=${config.matomoSiteId}&period=${period}&date=${date}&token_auth=${config.matomoToken}&filter_limit=-1`;
-  const devicesApiUrl = `${config.matomoSiteUrl}/index.php?module=API&method=DevicesDetection.getType&format=json&idSite=${config.matomoSiteId}&period=${period}&date=${date}&token_auth=${config.matomoToken}&filter_limit=-1`;
-  const screensApiUrl = `${config.matomoSiteUrl}/index.php?module=API&method=Resolution.getResolution&idSite=${config.matomoSiteId}&period=${period}&date=${date}&format=json&token_auth=${config.matomoToken}&filter_limit=-1`;
-  const pageUrlsViewsApiUrl = `${config.matomoSiteUrl}/index.php?module=API&method=Actions.getPageUrls&flat=1&format=json&idSite=${config.matomoSiteId}&period=${period}&date=${date}&token_auth=${config.matomoToken}&filter_limit=-1`;
+
+  const methods = [
+    'Actions.getPageUrls&expanded=1',
+    'DevicesDetection.getOsFamilies',
+    'DevicesDetection.getBrowsers',
+    'DevicesDetection.getType',
+    'Resolution.getResolution',
+    'Actions.getPageUrls&flat=1',
+  ];
+
+  const urls = methods.map(
+    (method) =>
+      `${config.matomoSiteUrl}/index.php?module=API&method=${method}&format=json&idSite=${config.matomoSiteId}&period=${period}&date=${date}&token_auth=${config.matomoToken}&filter_limit=-1`
+  );
 
   try {
-    const os = await requestAndReturn(osApiUrl);
-    const pages = await requestAndReturn(pagesApiUrl);
-    const screens = await requestAndReturn(screensApiUrl);
-    const devices = await requestAndReturn(devicesApiUrl);
-    const browsers = await requestAndReturn(browsersApiUrl);
-    const pageUrlsViews = await requestAndReturn(pageUrlsViewsApiUrl);
+    const [pagesResponse, osResponse, browsersResponse, devicesResponse, screensResponse, pageUrlsViewsResponse] =
+      await axios.all(urls.map((url) => axios.get(url)));
+
+    const pages = pagesResponse.data;
+    const os = osResponse.data;
+    const browsers = browsersResponse.data;
+    const devices = devicesResponse.data;
+    const screens = screensResponse.data;
+    const pageUrlsViews = pageUrlsViewsResponse.data;
+
+    if ([pages, os, browsers, devices, screens, pageUrlsViews].some((response) => response.result === 'error')) {
+      throw {
+        error: 'Error from Matomo API',
+        message: [pages, os, browsers, devices, screens, pageUrlsViews].find((response) => response.result === 'error')
+          ?.message,
+      };
+    }
 
     return res.status(200).json({ os, pages, screens, devices, browsers, pageUrlsViews });
   } catch (error) {
-    return res.status(500).json(error);
+    return res.status(500).json({
+      error: 'Internal server error',
+      details: error,
+    });
   }
 }
