067f25cc350da6d26a18791077d85a54a4a0996d
kiko-g <kikojpgoncalves@gmail.com>
2023-06-21 14:42:11 +0100

    fix: hosts script

--- hosts.sh
+++ hosts.sh
@@ -3,14 +3,20 @@ CONTAINER_NAME=$(docker ps --format '{{.Names}}' | grep matomo)
 CONFIG_FILE="/var/www/html/config/config.ini.php"
 NEW_HOSTS_LINE="trusted_hosts[] = \"localhost:31089\"\ntrusted_hosts[] = \"vmdsdev01:31089\""
 
-if [ -n "$CONTAINER_NAME" ]; then
-    if docker exec "$CONTAINER_NAME" test -f "$CONFIG_FILE"; then
-        docker exec -i "$CONTAINER_NAME" sed -i "/^trusted_hosts\[\]/d" "$CONFIG_FILE"
-        echo -e "$NEW_HOSTS_LINE" | docker exec -i "$CONTAINER_NAME" tee -a "$CONFIG_FILE" >/dev/null
-        echo "Trusted hosts updated successfully."
+if [ "$1" == "docker" ]; then
+    if [ -n "$CONTAINER_NAME" ]; then
+        if docker exec "$CONTAINER_NAME" test -f "$CONFIG_FILE"; then
+            docker exec -i "$CONTAINER_NAME" sed -i "/^trusted_hosts\[\]/d" "$CONFIG_FILE"
+            echo -e "$NEW_HOSTS_LINE" | docker exec -i "$CONTAINER_NAME" tee -a "$CONFIG_FILE" >/dev/null
+            echo "Trusted hosts updated successfully inside the Docker container."
+        else
+            echo "Config file '$CONFIG_FILE' not found inside the Matomo container. Have you finished the Matomo setup yet?"
+        fi
     else
-        echo "Config file '$CONFIG_FILE' not found inside the Matomo container. Have you finished the Matomo setup yet?"
+        echo "Matomo container not found. Make sure it is running and check the container name."
     fi
 else
-    echo "Matomo container not found. Make sure it is running and check the container name."
+    sed -i "/^trusted_hosts\[\]/d" "$CONFIG_FILE"
+    echo -e "$NEW_HOSTS_LINE" >> "$CONFIG_FILE"
+    echo "Trusted hosts updated successfully outside the Docker container."
 fi
