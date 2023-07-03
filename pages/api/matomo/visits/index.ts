40124544904d69d686660ec5fb70366af93f756b
kiko-g <kikojpgoncalves@gmail.com>
2023-07-04 00:12:54 +0100

    fix: page routes name in visits table

--- pages/api/matomo/visits/index.ts
+++ pages/api/matomo/visits/index.ts
@@ -63,10 +63,14 @@ export default async function getAllEvents(req: NextApiRequest, res: NextApiResp
       value: item.nb_visits,
     }));
 
-    const pagesFlat = pageViewsFlatResponse.data.map((item: any) => ({
-      name: item.label.startsWith('/') ? item.label.slice(1) : item.label,
-      value: item.nb_visits,
-    }));
+    const pagesFlat = pageViewsFlatResponse.data.map((item: any) => {
+      const value = item.nb_visits as number;
+      let label = item.label as string;
+      label.replace('apps/MES/', '');
+      const name = label.startsWith('/') ? label.slice(1) : label;
+
+      return { name, value };
+    });
 
     if ([os, browsers, devices, screens, pagesExpanded, pagesFlat].some((response) => response.result === 'error')) {
       throw {
